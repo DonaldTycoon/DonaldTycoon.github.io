500fa1c6da81b1a0d0b21b14f68efa609d612ca8
DonaldTycoon <d.brill98@gmx.de>
2020-01-12 21:58:56 +0100

    bugfixes3

--- script.js
+++ script.js
@@ -123,9 +123,9 @@ function getStats(info, save){
 function getSkills(data, save){
         save += '<div id="title">Skills</div>';
         save += '<div id="skill-title">Business Level</div>';
-        save += '<div class="progress">';
+        save += '<div class="progress-two">';
         save += '<div class="bar" id="bar_uncompleted"></div>';
-        save += '<div class="bar-two" style="background-color: #00264D"></div>';
+        save += '<div class="bar-two" style="background-color: #0063CA4d"></div>';
         save += '<div id="percent_skills">Business | Level ' + levelFromExp(Math.round(parseFloat(data.business.business))) + '  / 100 <i>(' + Math.round(parseFloat(data.business.business)) + ' EXP)</i></div >';
         save += '</div>';
         save += '</div>';
