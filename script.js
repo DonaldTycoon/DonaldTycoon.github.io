6e8b87f9d973c37e520b399fe5b93b332beeec13
DonaldTycoon <d.brill98@gmx.de>
2020-01-13 20:04:39 +0100

    f

--- script.js
+++ script.js
@@ -287,7 +287,7 @@ function percentage(g_Exp) {
             console.log(data[i-1]);
             console.log(data[i]- data[i-1]);
             console.log(g_Exp - data[i-1]);
-            console.log(((data[i]- data[i-1])/(g_Exp - data[i-1]))*100);
+            console.log(((g_Exp - data[i-1])/(data[i]- data[i-1]))*100);
             return ((g_Exp - data[i-1])/(data[i]- data[i-1]))*100;
         }
     }
