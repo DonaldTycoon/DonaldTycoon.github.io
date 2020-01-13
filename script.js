accd8e00960c3d8e8e3be398ac1a8ae65e0345b7
DonaldTycoon <d.brill98@gmx.de>
2020-01-13 13:18:31 +0100

    c

--- script.js
+++ script.js
@@ -252,6 +252,7 @@ function getSkills(data, save){
 function inventory(data, save) {
     save += '<div id="Inventory">';
     save += '<div id="title">Inventory</div>';
+    console.log(data.inventory.length);
     for (var i = 0; i < data.inventory.length; i++) {
         save += data.inventory[i];
     }
