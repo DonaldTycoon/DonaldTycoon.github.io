e34a30310f191565941918ea8e7fdd3c9f21a10c
DonaldTycoon <d.brill98@gmx.de>
2020-01-13 18:46:06 +0100

    f

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ function toHTML(info, value){
                 setTimeout(function(){
                     inventory(getSkills(info.data, getStats(info, getID(info, save))), value)
                 }, 3000);
-            );
+            ));
         }))
         .then(new Promise(function(resolve, reject) {
         console.log(save);
