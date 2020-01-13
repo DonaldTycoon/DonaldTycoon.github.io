b80661386198f188dec9f03317eeb2bbe6f026b8
DonaldTycoon <d.brill98@gmx.de>
2020-01-13 13:38:10 +0100

    c f

--- script.js
+++ script.js
@@ -256,6 +256,8 @@ function inventory(data, save) {
     console.log(JSON.stringify(data.inventory)[0]);
     console.log(JSON.stringify(data.inventory[0]));
     console.log(data.inventory[0]);
+    console.log(data.inventory[1]);
+    console.log(data.inventory[2]);
     for (var i = 0; i < Object.keys(data.inventory).length; i++) {
         save += '<div id="item">';
         save += JSON.stringify(data.inventory)[i];
